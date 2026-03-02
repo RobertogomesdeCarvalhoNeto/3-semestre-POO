1a4f7fc4493843e4e82654de5ffcdd756f154fe1
Roberto <robertgomes.neto@gmail.com>
2026-03-02 13:36:06 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+         class Pessoa{
+            String nome;
+            int idade;
+        }
+        Pessoa p1=new Pessoa();{
+            p1.nome="Roberto";
+            p1.idade=20;
+        }
+            System.out.println("Nome:"+p1.nome+"\n"+"Idade:"+p1.idade);
+        
+    }
+}
