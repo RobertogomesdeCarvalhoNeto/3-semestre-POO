9726dfa8f775db3a9ebf8c7172070eebb4138ee6
Roberto <robertgomes.neto@gmail.com>
2026-03-02 19:34:00 -0300

    scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Nome:");
+        String nome=sc.nextLine();
+        System.out.print("Idade:");
+        
+
          class Pessoa{
             String nome;
             int idade;
         }
+       
         Pessoa p1=new Pessoa();{
             p1.nome="Roberto";
             p1.idade=20;
